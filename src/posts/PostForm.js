3bc6cf409e66554683f7a43f60b9581c69f44744
David Thorpe <davidthorpe878@gmail.com>
2018-08-14 22:07:12 +0100

    updated mutation

--- src/posts/PostForm.js
+++ src/posts/PostForm.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
 
 class PostForm extends Component {
+  state = {
+    title: "",
+    body: ""
+  }
+
+  handleInput = (e) => {
+    const formData = {};
+    formData[e.target.name] = e.target.value;
+    this.setState({
+      ...formData
+    })
+  }
+
   render() {
+    const { onSubmit } = this.props;
+    const { title, body } = this.state;
     return (
-      <form>
-        <input type="text" placeholder="title" />
-        <textarea type="text" placeholder="body" />
+      <form onSubmit={e => {
+        e.preventDefault();
+        onSubmit({
+          variables: {
+            title,
+            body
+          }
+        }).then(() => {
+          this.setState({
+            title: "",
+            body: ""
+          })
+        }).catch((err) => {
+          console.log(err)
+        })
+      }}>
+        <input
+          type="text"
+          onChange={this.handleInput}
+          value={title}
+          name="title"
+          placeholder="title" />
+        <textarea
+          type="text"
+          onChange={this.handleInput}
+          value={body}
+          name="body"
+          placeholder="body" />
         <button>Submit</button>
       </form>
     );
